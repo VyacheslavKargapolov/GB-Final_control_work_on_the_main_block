6aab4a843fa6ea5906d4121bda12833a838c1b6f
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-07-11 19:04:48 +0500

    Добавил программу

---
+++ Kod_programmi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+string[] GetArrayFromString(string elements)
+
+{
+string[] rezult = elements.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+return rezult;
+}
+
+
+string[] GetNewArray(string[] arr, int b)
+{ string [] rez = new string[b] ;
+ int j = 0;
+ for (int i = 0; i< arr.Length; i++)
+ {
+     if (arr[i].Length <=3)  
+     {
+        rez[j] = arr[i];
+         
+         j++;
+     }
+    
+ }
+ 
+ return rez;
+}
+
+
+int CountElement(string[] arr)
+{
+int count = 0;
+for(int i=0; i < arr.Length; i++)
+    if (arr[i].Length <=3) count++;
+return count;
+}
+
+Clear();
+Write("Введите строки через пробел: ");
+
+string element = ReadLine();
+
+string[] array = GetArrayFromString(element);
+
+int size = CountElement(array);
+
+string[] array2 = GetNewArray(array, size);
+
+WriteLine();
+Write($"[{String.Join(", ", array)}] ---> ");
+WriteLine($"[{String.Join(", ", array2)}]");
+WriteLine();
